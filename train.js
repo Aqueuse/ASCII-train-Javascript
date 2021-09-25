46eefeaa6ac8dc096fae21c2e5e45f4e3e19cb34
llo <llo@DESKTOP-TFOARBH.localdomain>
2021-09-25 20:06:49 +0200

    better the train station

--- train.js
+++ train.js
@@ -1,50 +1,83 @@
-var trainLeftPos = 4000;
-var trainOrientation = "right";
-var speed = 16;
-var trainTimer = 300;
+let trainLeftPos = 4000;
+let trainOrientation = "right";
+let speed = 16;
+let trainTimer;
+let trainTimerDuration = 60;
+let trainState = "FULLSPEED";
 
-function update(progress) {
-    var windowWidth = window.innerWidth*8;
+function update() {
+    let windowWidth = window.innerWidth*8;
+    let middleScreen = windowWidth/2;
 
     let trainWidth = document.getElementById("train").offsetWidth;
+    let trainMedian = trainLeftPos-(trainWidth/2);
+
+    let stationWidth = document.getElementById("station").offsetWidth/8;
+    let leftStationBound = middleScreen-(stationWidth*2);
+    let rightStationBound = middleScreen+(stationWidth*2);
 
     // orientation
-    if (trainLeftPos > windowWidth+trainWidth*1.1) {
+    if (trainLeftPos+trainWidth >= windowWidth+(trainWidth*3)) {
         document.getElementById("train").style.transform = "scaleX(-1)";
         trainOrientation = "left";
     }
 
-    if (trainLeftPos <= -trainWidth*1.5) {
+    if (trainLeftPos <= -(trainWidth*1.5)) {
         document.getElementById("train").style.transform = "scaleX(1)";
         trainOrientation = "right";
     }
 
     // speed
-    if (trainTimer > 0) {
-        if (trainLeftPos > windowWidth/4 && trainLeftPos < (windowWidth/4)*3 && speed > 0) {
-            speed = speed-0.1;
-            trainTimer = trainTimer-1;
-        }
-        else {
+    if (trainState != "FREINAGE" && speed >= 16) {
+        trainState = "FULLSPEED"
+    }
+
+    // le train entre en gare par la gauche
+    if (trainMedian > leftStationBound && trainMedian < middleScreen && trainOrientation == "right") {
+        trainState = "FREINAGE";
+        trainTimer = trainTimerDuration;
+    }
+
+    // le train entre en gare par la droite
+    if (trainMedian < rightStationBound && trainMedian > middleScreen && trainOrientation == "left") {
+        trainState = "FREINAGE";
+        trainTimer = trainTimerDuration;
+    }
+
+    if (speed <= 0.1 && trainTimer > 0) {
+        trainState = "STOP";
+    }
+
+    if (trainTimer == 0 && trainState == "STOP") {
+        trainState = "ACCELERATION";
+    }
+
+    switch(trainState) {
+        case "FULLSPEED":
             speed = 16;
-        }
-    }
-
-    if (trainTimer == 0 && speed < 16) {
-        speed = speed+0.1;
-    }
-
-    else {
-        trainTimer = 30;
+            break;
+        case "STOP":
+            speed = 0;
+            trainTimer = trainTimer-1;
+            break;
+        case "FREINAGE":
+            speed = speed-0.2;
+            break;
+        case "ACCELERATION":
+            speed = speed+0.2;
+            break;
+        default:
+            speed = 16;
+            break;
     }
 
     // final move
     if (trainOrientation == "left") {
-        trainLeftPos = (trainLeftPos-speed);
+        trainLeftPos = trainLeftPos-speed;
     }
 
     if (trainOrientation == "right") {
-        trainLeftPos = (trainLeftPos+speed);
+        trainLeftPos = trainLeftPos+speed;
     }
 
     document.getElementById("train").style.left = trainLeftPos+"px";
@@ -54,14 +87,14 @@ function update(progress) {
   }
   
   function loop(timestamp) {
-    var progress = timestamp - lastRender
+    var progress = timestamp - lastRender;
   
-    update(progress)
-    draw()
+    update(progress);
+    draw();
   
-    lastRender = timestamp
-    window.requestAnimationFrame(loop)
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
   }
 
-  var lastRender = 0
-  window.requestAnimationFrame(loop)
+  var lastRender = 0;
+  window.requestAnimationFrame(loop);
